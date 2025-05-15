3d8157fc4d3aff908f79a9cc2414a93f15d1a1ad
Jus-Zhang <justinjz.sd@gmail.com>
2025-05-15 10:11:11 -0700

    JZ commit

--- src/Pile.java
+++ src/Pile.java
@@ -1,10 +1,11 @@
-public class Pile{
-	
-	private Card arrCard;
-	private Player myPlayer;
+import java.util.ArrayList;
+
+public class Pile
+{
+	private ArrayList<Card> cardList;
 	
 	public Pile() {
-		
+		cardList = new ArrayList<Card>();
 	}
 	
 	public void drawFromPile() {
